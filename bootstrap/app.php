a36df3e53922c792a361ab6ada7beca316a87324
Thiago Matheus <thiagoma369@gmail.com>
2026-04-27 18:32:33 -0300

    fix: voltar pro codigo original

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,12 +15,13 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
-  ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions): void {
 
-    $exceptions->render(function (Illuminate\Auth\AuthenticationException $e, $request) {
-        return response()->json([
-            'message' => 'Não autenticado'
-        ], 401);
-    });
+        $exceptions->render(function (AuthenticationException $e, $request) {
+            return response()->json([
+                'message' => 'Não autenticado'
+            ], 401);
+        });
 
-});
+    })
+    ->create();
