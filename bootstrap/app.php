e1e35d1a0d4c673813473b393d74943206081656
Thiago Matheus <thiagoma369@gmail.com>
2026-04-28 17:21:14 -0300

    teste

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,13 +14,17 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
-    ->withExceptions(function ($exceptions) {
+   ->withExceptions(function ($exceptions) {
 
-        $exceptions->render(function (AuthenticationException $e, $request) {
+    $exceptions->render(function (\Illuminate\Auth\AuthenticationException $e, $request) {
+        
+        if ($request->is('api/*')) {
             return response()->json([
                 'message' => 'Não autenticado'
             ], 401);
-        });
+        }
 
-    })
-    ->create(); 
+    });
+
+})
+->create(); 
